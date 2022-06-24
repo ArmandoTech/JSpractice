f34cba3af075b3511216adf0a111a67021838b0e
Armando Arenas <armandoarenass@hotmail.com>
2022-06-24 11:01:52 -0500

    mode

---
+++ mode.js
@@ -0,0 +1,19 @@
+
+const list= [1,2,3,1,2,1,1,1,3,1,1,2,3,2,4,3,3];
+const countList={};
+
+list.map(function(element) {
+    if (countList[element]) {
+        countList[element] += 1;
+    } else{
+        countList[element]= 1;
+    }
+});
+
+const listArray= Object.entries(countList).sort(
+    function(a, b){
+        return a[1]-b[1];
+    }
+);
+
+mode= listArray[listArray.length - 1];
